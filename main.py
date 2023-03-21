6aaf6a971cb2a5f4bbb42e4b505785900dffd36d
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2023-03-21 21:40:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
+from revChatGPT.V3 import Chatbot
+import logging
+import os
+import asyncio
+
+print('Бот запущен!')
+
+logging.basicConfig(level=logging.INFO)
+
+chatbot = Chatbot(api_key="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")
+TOKEN = token ='5747127396:AAEXYrvUMht9LJqUzxoQA-Xg4Tv8wO98tRI'
+
+router = Router()
+
+@router.message(Command(commands=["clear"]))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(text='История очищена!')
+    chatbot.reset()
+
+
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    await message.answer( text='Обрабатываю запрос, пожалуйста подождите!')
+    result = chatbot.ask(message.text)
+    await message.answer(text=result)
+
+async def main() -> None:
+    dp = Dispatcher()
+    dp.include_router(router)
+    bot = Bot(TOKEN, parse_mode="HTML")
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+
